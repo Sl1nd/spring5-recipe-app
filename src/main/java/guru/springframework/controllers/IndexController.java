04f64271624c71109f641acb4e0d13f218e6f989
Slindauer <sven.lindauer@inovex.de>
2022-08-06 07:24:41 +0200

    Add query methods

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,16 +1,33 @@
 package guru.springframework.controllers;
 
+import guru.springframework.domain.Category;
+import guru.springframework.domain.UnitOfMeasure;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitToMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 /**
  * Created by jt on 6/1/17.
  */
 @Controller
 public class IndexController {
+  private final CategoryRepository categoryRepository;
+  private final UnitToMeasureRepository unitOfMeasureRepo;
+
+  public IndexController(CategoryRepository categoryRepository, UnitToMeasureRepository unitOfMeasureRepo) {
+    this.categoryRepository = categoryRepository;
+    this.unitOfMeasureRepo = unitOfMeasureRepo;
+  }
 
   @RequestMapping({"", "/", "/index"})
   public String getIndexPage(){
+    Optional<Category> category = categoryRepository.findByDescription("Mexican");
+    Optional<UnitOfMeasure> uom = unitOfMeasureRepo.findByDescription("Teaspoon");
+    System.out.println("CAT ID is:" + category.get().getId());
+    System.out.println("Uom ID is:"+ uom.get().getId());
     return "index";
   }
 }

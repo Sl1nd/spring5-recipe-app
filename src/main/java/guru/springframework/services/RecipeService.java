0ca4d50291ef4c84798644cc0685a42ba1b32695
Slindauer <sven.lindauer@inovex.de>
2022-08-18 08:18:45 +0200

    Add commands as well as converters

--- src/main/java/guru/springframework/services/RecipeService.java
+++ src/main/java/guru/springframework/services/RecipeService.java
@@ -1,5 +1,6 @@
 package guru.springframework.services;
 
+import guru.springframework.commands.RecipeCommand;
 import guru.springframework.domain.Recipe;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.core.CrudMethods;
@@ -10,4 +11,6 @@ public interface RecipeService  {
   Set<Recipe> getRecipes();
 
   Recipe findById(Long ID);
+
+  RecipeCommand saveRecipe(RecipeCommand recipeCmd);
 }
--- src/main/java/guru/springframework/services/RecipeServiceImplementation.java
+++ src/main/java/guru/springframework/services/RecipeServiceImplementation.java
@@ -1,9 +1,13 @@
 package guru.springframework.services;
 
+import guru.springframework.commands.RecipeCommand;
+import guru.springframework.converters.RecipeCommandToRecipe;
+import guru.springframework.converters.RecipeToRecipeCommand;
 import guru.springframework.domain.Recipe;
 import guru.springframework.repositories.RecipeRepository;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
@@ -11,11 +15,14 @@ import java.util.Set;
 @Service
 public class RecipeServiceImplementation implements RecipeService {
   private final RecipeRepository recipeRepository;
+  private final RecipeCommandToRecipe recipeCmdToRecipe;
+  private final RecipeToRecipeCommand recipeToRecipeCmd;
 
-  public RecipeServiceImplementation(RecipeRepository recipeRepository) {
+  public RecipeServiceImplementation(RecipeRepository recipeRepository, RecipeCommandToRecipe recipeCmdToRecipe, RecipeToRecipeCommand recipeToRecipeCmd) {
     this.recipeRepository = recipeRepository;
+    this.recipeCmdToRecipe = recipeCmdToRecipe;
+    this.recipeToRecipeCmd = recipeToRecipeCmd;
   }
-
 
   @Override
   public Set<Recipe> getRecipes() {
@@ -31,5 +38,13 @@ public class RecipeServiceImplementation implements RecipeService {
       throw new RuntimeException("Recipe Not Found");
     }
     return recipe.get();
+  }
+
+  @Override
+  @Transactional
+  public RecipeCommand saveRecipe(RecipeCommand recipeCmd) {
+    Recipe detachedRecipe = recipeCmdToRecipe.convert(recipeCmd);
+    Recipe savedRecipe = recipeRepository.save(detachedRecipe);
+    return recipeToRecipeCmd.convert(savedRecipe);
   }
 }
